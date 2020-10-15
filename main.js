06a89125331c849ec43cc440975505f97f6fb9d7
bogdan dima <bogdan_dima@ukr.net>
2020-10-15 21:56:55 +0300

    9 lesson

---
+++ main.js
@@ -0,0 +1,30 @@
+let userFirstName = "Tom",
+    userSecondName = "Crus",
+    marigeStatus = 0,
+    isActor = true,
+    currentYear = 2017,
+    birth = 1967;
+
+    //messarges;
+
+let mainMassage = "This person is",
+    helloMsg = "Hello,",
+    ageMsgFirst = "Person is",
+    ageMsgLast = "year old. \n",
+    spaceMsg = " ",
+    doAnBreak = ". \n";
+
+    //бизнесс логика;
+let resultMsg = null,
+    actorMst = "",
+    age = 0;
+resultMsg = helloMsg + userFirstName + spaceMsg + userSecondName + doAnBreak; //операторы;
+if(isActor){
+    
+}
+console.log(resultMsg);
+
+
+
+
+
